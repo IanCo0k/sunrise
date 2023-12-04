31a50183a17da916ec25d760c16ed75e6470cbff
IanCo0k <ianscook19@gmail.com>
2023-12-04 12:03:19 -0500

    unihiding the config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPE5t0YfXiIWOnoLZC86d_jhZmMz8qFDE",
+    authDomain: "sunrise-tech-bce74.firebaseapp.com",
+    projectId: "sunrise-tech-bce74",
+    storageBucket: "sunrise-tech-bce74.appspot.com",
+    messagingSenderId: "278030644240",
+    appId: "1:278030644240:web:00b81cca90c3459fe18695",
+    measurementId: "G-06288RF45X"
+  };
+
+export default firebaseConfig;
