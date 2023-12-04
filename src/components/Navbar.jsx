51761defabed19a24bc1dfd2157e1337feca3d74
IanCo0k <ianscook19@gmail.com>
2023-12-04 13:33:21 -0500

    update to use Link component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Navbar = () => {
     const auth = getAuth();
@@ -30,10 +31,6 @@ const Navbar = () => {
         setDropdownOpen(false);
     };
 
-    const handleClickProfile = () => {
-        setDropdownOpen(!dropdownOpen);
-    };
-
     const handleLogout = () => {
         signOut(auth)
             .then(() => {
@@ -55,13 +52,15 @@ const Navbar = () => {
 
     return (
         <nav className="bg-blue-500 p-4 flex justify-between items-center">
-            <div className="text-white font-bold text-lg">SunRise Tech</div>
+            <Link to="/" className="text-white font-bold text-lg">
+                SunRise Tech
+            </Link>
             <div className="flex items-center space-x-4">
                 <div className="text-white flex items-center">
                     Hi, {displayName}
                     <div
                         className="relative inline-block ml-2"
-                        onClick={handleClickProfile}
+                        onClick={toggleDropdown}
                         onBlur={closeDropdown}
                     >
                         <img
@@ -72,37 +71,21 @@ const Navbar = () => {
                         {dropdownOpen && (
                             <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg border border-gray-300">
                                 {/* Dropdown menu items */}
-                                <a
-                                    href="/about"
-                                    className="block px-4 py-2 text-gray-800 hover:bg-blue-100"
-                                >
+                                <Link to="/about" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">
                                     About
-                                </a>
-                                <a
-                                    href="/calendar"
-                                    className="block px-4 py-2 text-gray-800 hover:bg-blue-100"
-                                >
+                                </Link>
+                                <Link to="/calendar" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">
                                     Calendar
-                                </a>
-                                <a
-                                    href="/wakeup"
-                                    className="block px-4 py-2 text-gray-800 hover:bg-blue-100"
-                                >
+                                </Link>
+                                <Link to="/wakeup" className="block px-4 py-2 text-gray-800 hover:bg-blue-100">
                                     Wake Up
-                                </a>
-                                <a
-                                    href="/config"
-                                    className="block px-4 py-2 text-gray-800 hover:bg-blue-100 hover:rounded"
-                                >
+                                </Link>
+                                <Link to="/config" className="block px-4 py-2 text-gray-800 hover:bg-blue-100 hover:rounded">
                                     Configure
-                                </a>
-                                <a
-                                    href="/"
-                                    className="block px-4 py-2 text-gray-800 hover:bg-blue-100"
-                                    onClick={handleLogout} // Update the onClick event to call handleLogout
-                                >
+                                </Link>
+                                <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-blue-100" onClick={handleLogout}>
                                     Log Out
-                                </a>
+                                </Link>
                             </div>
                         )}
                     </div>
